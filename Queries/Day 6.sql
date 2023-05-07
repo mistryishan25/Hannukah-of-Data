c0653c4ad30d48b4799b74f8c224750e417f0039
Ishan Mistry <52032248+mistryishan25@users.noreply.github.com>
2023-05-07 19:22:09 +0530

    Update Day 6.sql

--- Queries/Day 6.sql
+++ Queries/Day 6.sql
@@ -1,112 +1,48 @@
+/*
+
+CONTEXT:
+- This query aims to highlight adn quantify the customer who has made been the most frugal with their shopping habits.
+
+RESULT EXPECTATION:
+- The SQL code returns the customer ID, name, phone number, total amount spent by the customer, and the profit earned by the customer.
+
+ASSUMPTIONS:
+- The customers, orders, order_items, and products tables exist and contain the relevant columns.
+- Profit = (unit price - wholesale) * quantity
+- There are no NULL values in the relevant columns.
+
+APPROACH:
+- Create a CTE profits_per_product that calculates the profit earned per unit and total profit for each order.
+- Join the orders table with the profits_per_product CTE and the customers table to get customer information for each order.
+- Group the results by customer ID and sum the total profit for each customer.
+- Order the results by profit in ascending order, and limit the output to the customer with the highest profit.
+*/
 
 
 
--- cleaning the customers table
-create temp table clean_info as (
+with profits_per_product as (
 	select
-		name,
--- First name
-	split_part(upper(name),' ' ,1) as firstname,
--- Middle name
-		case 
-			when length(name) - length( replace(name, ' ', '')) = 1 
-			then null
-			else split_part(upper(name),' ' ,2) 
-		end	as middlename,
--- Last Name
-		case 
---  		there are only 2 parts to the name 
-			when length(name) - length( replace(name, ' ', '')) = 1 
-			then split_part(upper(name),' ' ,2)
-			else split_part(upper(name),' ' ,3)
-		end as lastname,
--- Cleaned phone number
-		replace(phone, '-', '') as phone_number,
--- customerid
-		customerid 
-	from customers
-);
+	orderid,
+	qty,
+	(unit_price - wholesale_cost)::decimal as per_unit_profit,
+	(unit_price - wholesale_cost)::decimal*qty as total_profit
+	from 
+		products p 
+		right join order_items i
+		on p.sku = i.sku
+)
 
-
-
--- Only later I realized that we also have middle name!
-select length(lastname), lastname 
-from clean_info 
-where length(lastname) <= (select max(length(lastname)) from clean_info) and 
-		length(lastname) >9 ;
-
-
--- Create a mapping from numbers to alphabets
-	-- A is 65 so we use row_numbers to generate a char seq
-	-- PQRS - 7 AND WXYZ- 9 
-	-- Change YZ-9, V-8, S-7   
-
-create temp table interim_seq as (
-	select 
-		case when letters = 'S' then 7
-			 when letters = 'V' then 8
-			 when letters = 'Y' then 9
-			 when letters = 'Z' then 9
-			 when letters = ' ' then 0 
-			 else int_map 
-		end as key_int,
-		letters
-	from (
-	select 
-		generate_series(0,25)/3 + 2 as int_map,
-		chr(generate_series(0,25)+65) as letters) as sub	
-);
-
-
-create temp table final_map as (
-	select 
-	letters,
-	key_int,
-	repeat(cast(key_int as char),cast(print_times as int)) as code
-from (
-	select
-		letters,
-		key_int,
-		row_number() over(partition by key_int) as print_times
-	from interim_seq 	
-	) as sub_seq
-);
-
-create temp table char_pos as (
-	select generate_series(1,10) as n 
-);
-
--- get the individual letters and the corresponding code
-create temp table coded_letters as (
-	select
-	distinct
-		customerid,
-		lastname,
-		phone_number,
-		n,
-		substring(lastname,n,1),
-		key_int
-	from char_pos
-		cross join clean_info
-		right join final_map
-		-- the seperate letters should match the code form mapping
-			on letters = substring(lastname, n, 1)
-	where n between 0 and length(lastname) and 
-		length(lastname) = 10
-	order by 1,2
-);
-
-
-create temp table final_suspect as (
-select 
-	customerid,
-	phone_number,
--- array_agg gives output as text so we need to convert to string 
-  	array_to_string(array_agg(key_int), '') as decoded_number
-from coded_letters
-group by customerid, phone_number
-having array_to_string(array_agg(key_int), '') = phone_number
-);
-	
-select * from customers where customerid in (select customerid from final_suspect)
-
+select
+	max(c.customerid) as customerid,
+	max(c.name),
+	max(c.phone),
+	max(o.total) total,
+	sum(total_profit)::decimal as profit
+from orders o
+join profits_per_product ppp 
+	on ppp.orderid = o.orderid
+join customers c
+	on o.customerid = c.customerid
+group by c.customerid
+order by profit
+limit 1
