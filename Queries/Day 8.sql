f4ace2b53b8a183cfdd42b7af62c881a35b62187
Ishan Mistry <52032248+mistryishan25@users.noreply.github.com>
2023-05-07 20:42:32 +0530

    Update Day 8.sql

--- Queries/Day 8.sql
+++ Queries/Day 8.sql
@@ -1,27 +1,38 @@
--- Hints : 
--- He owns an entire set of Noah’s collectibles! 
+/*
 
--- Just to get an idea of all the collectibles
+CONTEXT:
+- This query is used to find the customer who ordered the most collectibles of all customers.
 
-select * 
-from products
-where sku like 'COL%'
+RESULT EXPECTATION:
+- The query will return the customer information for the person with the most collectibles.
 
--- Let's find the customer that ordered the most collectibles of them all.
+ASSUMPTIONS:
+- The SKU for all collectibles starts with 'COL'.
+- The customer information is stored in the 'customers' table.
+- The order information is stored in the 'orders' table, with order ID as the primary key.
+- The items associated with each order are stored in the 'order_items' table, with order ID and SKU as foreign keys.
+
+APPROACH:
+- Filter orders that have COL items 
+- Group by customerid and count the qty of the
+
+*/
+
+
 with suspect as (
 select 
 	o.customerid,
 	sum(qty) as total_col_items
-	
 from order_items oi
-join orders o
-on o.orderid = oi.orderid
+	join orders o
+	on o.orderid = oi.orderid
 where substring(sku, 1,3) = 'COL'
 group by o.customerid
 order by total_col_items desc
 limit 1 
 )
 	
+
 -- getting the phone number of the last guy
 select * from customers
 where customerid in (select customerid from suspect)
