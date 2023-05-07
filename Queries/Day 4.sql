d2ce109ae86486077e2626ccc3c8080e1fc8e72c
Ishan Mistry <52032248+mistryishan25@users.noreply.github.com>
2023-05-07 19:02:50 +0530

    Update Day 4.sql

--- Queries/Day 4.sql
+++ Queries/Day 4.sql
@@ -1,10 +1,22 @@
--- Hints : 
--- Claims the first pastries that came out of the oven
--- Came over to help at 5AM
+/* 
 
--- Group and count if there is at least on BKY item in the orders
--- order by the time desc
--- select the earliest ones
+CONTEXT:
+- This SQL code is intended to find the customer who made the most orders between 4:00 am and 5:00 am and purchased at least one bakery product.
+
+RESULT EXPECTATION:
+- The result should display the customer ID, phone number, and the number of orders made between 4:00 am and 5:00 am.
+
+ASSUMPTION:
+- It is assumed that the substring 'BKY' is used to identify bakery products.
+- It is given that the customer always picked up the first batch of pastries straight from the oven
+
+APPROACH:
+- A CTE is used to filter out orders that have at least one bakery product.
+- Another CTE is used to rank the orders by the time they were shipped.
+- Finally, the ranked orders are joined with the customers table to retrieve the phone number of each customer, and then grouped by customer ID and phone number to count the number of orders made between 4:00 am and 5:00 am. 
+- The result is then sorted by the number of orders in descending order and limited to the customer with the most orders. 
+
+*/
 
 with bky_orders as (
 	select
@@ -23,17 +35,18 @@ select
 	extract( hour from o.shipped) as hour,
 	rank() over(partition by shipped::date order by shipped::time asc)
 from bky_orders as bo
-inner join orders as o
-on o.orderid = bo.orderid
+	inner join orders as o
+	on o.orderid = bo.orderid
 )
 
 
-select c.customerid,
-c.phone,
-count(*) 
+select 
+	c.customerid,
+	c.phone,
+	count(*) 
 from ranked_bky_orders as rbo
-inner join customers as c
-on c.customerid = rbo.customerid 
+	inner join customers as c
+	on c.customerid = rbo.customerid 
 where rank=1 and rbo.hour between 4 and 5
 group by c.customerid, c.phone
 order by count(*) desc
